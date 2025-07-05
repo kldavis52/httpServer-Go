f700b9c5d56b54168bdc558d08314c8c85094c90
Kyle Davis <kyledavis@Kyles-MacBook-Pro.local>
2025-07-05 19:02:56 -0400

    server internals

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
@@ -29,7 +30,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) Run() error {
-	addr := fmt.Sprint(":%d", s.port)
+	addr := fmt.Sprintf(":%d", s.port)
 	s.server = &http.Server{
 		Addr:    addr,
 		Handler: s,
@@ -37,4 +38,24 @@ func (s *Server) Run() error {
 
 	fmt.Printf("Server starting on port %d\n", s.port)
 	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.server.Shutdown(ctx)
+}
+
+func (s *Server) GetRouter() *Router {
+	return s.Router
+}
+
+type ErrorHandler func(w http.ResponseWriter, r *http.Request, err error)
+
+func WithErrorHandler(handler ErrorHandler) ServerOption {
+	return func(s *Server) {
+		s.errorHandler = handler
+	}
+}
+
+func DefaultErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
